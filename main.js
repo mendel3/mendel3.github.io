05505ad305902214d3e3c3f3c1639cc8414afbd9
myles sloan <mylessloansf@gmail.com>
2017-12-12 12:56:39 -0800

    Gf

--- main.js
+++ main.js
@@ -15,3 +15,6 @@ function buyCursor(){
     var nextCost = Math.floor(10 * Math.pow(1.1,cursors));       //works out the cost of the next cursor
     document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
+window.setInterval(function(){
+cookieClick(cursors);
+}, 1000);
